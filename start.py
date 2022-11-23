30765e422149ffc0f333868802797a05a347e327
James Bathgate <james@searchspring.com>
2022-11-22 20:56:49 -0700

    Open and close file when writing to ensure streamlabs can read it.

--- start.py
+++ start.py
@@ -2,26 +2,26 @@ from TikTokLive import TikTokLiveClient
 from TikTokLive.types.events import SubscribeEvent, FollowEvent
 
 # UPDATE THESE TO BE WHERE YOU WANT FILES WRITTEN
-subscribersLog = "C:/Path/To/Your/SubscribersFile.txt"
-followersLog = "C:/Path/To/Your/FollowersFile.txt"
-streamerId = "@badaimbenny"
+subscribersLog = "/Users/jbathgate/Desktop/subscribers.txt"
+followersLog = "/Users/jbathgate/Desktop/followers.txt"
+streamerId = "@birdmann86"
 
 # Instantiate the client with the user's username
 client: TikTokLiveClient = TikTokLiveClient(unique_id=streamerId, **({"enable_extended_gift_info": True}))
 
-subscribersFile = open(subscribersLog, "a+")
-followersFile = open(followersLog, "a+")
-
 @client.on("subscribe")
 async def on_connect(event: SubscribeEvent):
     print(f"{event.user.nickname} subscribed")
+    subscribersFile = open(subscribersLog, "a+")
     subscribersFile.write(f"{event.user.nickname}\n")
+    subscribersFile.close();
 
 @client.on("follow")
 async def on_follow(event: FollowEvent):
     print(f"{event.user.nickname} followed")
+    followersFile = open(followersLog, "a+")
     followersFile.write(f"{event.user.nickname}\n")
-
+    followersFile.close();
 
 if __name__ == '__main__':
     client.run()
